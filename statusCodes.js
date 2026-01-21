20133e8b023390a41a25eb14028e567829efd300
Teresa Lew <teresalew92@gmail.com>
2026-01-21 10:31:19 -0800

    add getStatusMessage utility to retrieve appropriate status message given status code as an input

---
+++ statusCodes.js
@@ -0,0 +1,13 @@
+export function getStatusMessage(statusCode) {
+  switch (statusCode) {
+    case 200:
+      return 'OK';
+    case 400:
+      return 'Bad Request';
+    case 401:
+      return 'Unauthorized';
+    case 500:
+      return 'Internal Server Error';
+  }
+  return 'OK';
+};
